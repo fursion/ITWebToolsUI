ef3eba8b4aed089e369c4ffe4b3ccc19092fdf51
fursion <604357968@qq.com>
2022-05-19 01:04:02 +0800

    update

---
+++ ITWebToolsUI/Moudle/StaffInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ITWebToolsUI.Moudle
+{
+	public class StaffInfo
+	{
+		public string UUID { get; set; }
+		public string Url { get; set; }
+		public string Name { get; set; }
+		public string PhoneNamber { get; set; }
+	}
+}
+
